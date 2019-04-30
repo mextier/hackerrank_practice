12627f76399eb94a24ba69831f0161bf7f41a633
mextier <34535363+mextier@users.noreply.github.com>
2019-04-30 16:37:52 +0600

    02

---
+++ 02_A Very Big Sum.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    return (sum(i for i in ar))
+
+
+input_data = [1000000001,1000000002,1000000003,1000000004,1000000005]
+print(aVeryBigSum(input_data))
