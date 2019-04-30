45a32d5e0f6f00e5f62c6e24bec57418674a0c11
mextier <34535363+mextier@users.noreply.github.com>
2019-04-30 12:11:26 +0600

    01

---
+++ 01_Compare the Triplets.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    d= list(zip(a,b))
+    score1 = len(list([1 for i in d if i[0]>i[1]]))
+    score2 = len(list([1 for i in d if i[0]<i[1]]))
+    return (score1,score2)
+
+print(compareTriplets(list([20,20,30]),list([15,20,35])))
+ 
