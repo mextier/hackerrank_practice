3aaf015eb0c27e98791f3bd7937235e20b7e5a22
mextier <mextier@gmail.com>
2019-05-02 00:04:33 +0600

    03

--- 03_Diagonal Difference.py
+++ 03_Diagonal Difference.py
@@ -11,17 +11,10 @@ def diagonalDifference(arr):
     size = len(arr[0])
     for _ in arr:
         data.extend(_)
-    sum1 = sum([d for d,i in enumerate(data) if i//size==0])
-    sum2 = 0
-    return abs(sum1+sum2)
+    sum1 = sum([d for i,d in enumerate(data) if i==int(i//size)*(size+1)])
+    sum2 = sum([d for i,d in enumerate(data) if int((i+1+i/size)%size)==0])
+    return abs(sum1-sum2)
 
-
+    
 input_data = [[11,2,4],[4,5,6],[10,8,-12]]
 print(diagonalDifference(input_data))
-
-""" n = int(input())
-arr = []
-for _ in range(n):
-    arr.append(list(map(int, input().rstrip().split())))
-print(arr)
- """
