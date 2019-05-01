924166c0ac03bae4c7f40894069ea99467c4d732
mextier <mextier@gmail.com>
2019-05-02 02:01:31 +0600

    alg

--- Alg_Grading Students.py
+++ Alg_Grading Students.py
@@ -2,8 +2,15 @@ import os
 import sys
 
 def gradingStudents(grades):
-    return(grades)
+    final = []
+    for grade in grades:
+        if grade<38 or grade%5 < 3:
+            final.append(grade)
+        else:
+            #if grade%5 > 0:
+            final.append(5*(grade//5)+ 5 if grade%5 > 0 else 0)
+    return(final)
 
 
-grades = []
+grades = [73,67,38,33]
 print(gradingStudents(grades))
---
+++ Alg_Kangaroo.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the kangaroo function below.
+def kangaroo(x1, v1, x2, v2):
+    return 1
+
+x1,v1,x2,v2 = 0,3,4,2
+print(kangaroo(x1, v1, x2, v2))
