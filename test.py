7c9985c5ce1a9f1f02924bb746caf1e77e7ef973
mextier <mextier@gmail.com>
2019-05-11 01:58:27 +0600

    test

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-n = 2
-if n & 1:
-    print('Weird')
-else:
-    if n in range(2,6):
-        print('Not Weird')
-    elif n in range(6,21):
-        print('Weird')
-    elif n > 20:
-        print('Not Weird')
+import sys
+
+
+n = int(input())
+data = dict()
+for i in range(n):
+    s1, s2 = str(input()).split(" ")
+    data[s1] = s2
+keys_ = list()
+for line in sys.stdin:
+    line = line.rstrip('\r\n')
+    keys_.append(line)
+for key in keys_:
+    if key in data.keys():
+        print(f"{key}={data[key]}")
+    else:
+        print("Not found")
+
+
+
+
